32aad7d6a6a3f008ac934af9ea3adbf62c400eb4
Ward Impe <ward.impe@dotnetlab.eu>
2023-01-14 20:23:31 +0100

    update module to be es6

--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dom_1 = require("@zero-dependency/dom");
-const fireworks_js_1 = require("fireworks-js");
-require("./style.css");
+import { el } from '@zero-dependency/dom';
+import { Fireworks } from 'fireworks-js';
+import './style.css';
 const app = document.querySelector('#app');
-const fireworks = new fireworks_js_1.Fireworks(app);
-const start = (0, dom_1.el)('button', {
+const fireworks = new Fireworks(app);
+const start = el('button', {
     onclick: () => {
         fireworks.start();
     }
 }, 'Start');
-const stop = (0, dom_1.el)('button', {
+const stop = el('button', {
     onclick: () => {
         fireworks.waitStop();
     }
 }, 'Stop');
-const launch = (0, dom_1.el)('button', {
+const launch = el('button', {
     onclick: () => {
         fireworks.launch(Number(count.value));
     }
 }, 'Launch');
-const count = (0, dom_1.el)('input', {
+const count = el('input', {
     value: '1',
     min: '1',
     max: '15',
@@ -30,7 +28,7 @@ const count = (0, dom_1.el)('input', {
         width: '2rem'
     }
 });
-const buttons = (0, dom_1.el)('div', {
+const buttons = el('div', {
     style: {
         position: 'absolute',
         display: 'flex',
