055c74e544e171330cfcdfdcafd821ba81cd81a0
Ward Impe <ward.impe@dotnetlab.eu>
2023-01-14 20:51:08 +0100

    meh

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
-import { Fireworks } from 'fireworks-js';
-import './style.css';
+"use strict";
+var exports = {};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dom_1 = require("@zero-dependency/dom");
+const fireworks_js_1 = require("fireworks-js");
+require("./style.css");
 const app = document.querySelector('#app');
-const fireworks = new Fireworks(app);
-const start = el('button', {
+const fireworks = new fireworks_js_1.Fireworks(app);
+const start = (0, dom_1.el)('button', {
     onclick: () => {
         fireworks.start();
     }
 }, 'Start');
-const stop = el('button', {
+const stop = (0, dom_1.el)('button', {
     onclick: () => {
         fireworks.waitStop();
     }
 }, 'Stop');
-const launch = el('button', {
+const launch = (0, dom_1.el)('button', {
     onclick: () => {
         fireworks.launch(Number(count.value));
     }
 }, 'Launch');
-const count = el('input', {
+const count = (0, dom_1.el)('input', {
     value: '1',
     min: '1',
     max: '15',
@@ -27,7 +31,7 @@ const count = el('input', {
         width: '2rem'
     }
 });
-const buttons = el('div', {
+const buttons = (0, dom_1.el)('div', {
     style: {
         position: 'absolute',
         display: 'flex',
